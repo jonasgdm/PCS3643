f39c57766122e4dd79be93354936477cf266f75b
jonasgdm <jonasgm@usp.br>
2022-10-20 15:21:22 -0300

    🚧 Inicializar estrutura de classes
    Co-authored-by: pedrobacic <pedrobacic@users.noreply.github.com>

---
+++ MyProj/book/classes/controllers.py
@@ -0,0 +1,31 @@
+from book.models import Voo
+import entities
+
+class ControladorCRUD():
+    def __init__(self):
+        pass
+    def cadastrarVoo(self):
+        pass
+    def consultarVoo(self):
+        pass
+    def atualizarVoo(self):
+        pass
+    def deletarVoo(self):
+        pass
+
+class ControladorMonitoracao():
+    def __init__(self):
+        pass
+
+    def atualizarStatus(self):
+        pass
+
+    def exibirVoo(self):
+        pass
+
+class ControladorRelatorios():
+    def __init__(self):
+        pass
+    
+    def gerarRelatorio(self, tipo):
+        pass
---
+++ MyProj/book/classes/interfaces.py
@@ -0,0 +1,15 @@
+from book.models import Voo
+import entities, controllers
+
+class InterfaceCRUD():
+    def __init__(self):
+        pass
+
+class InterfaceMonitoracao:
+    def __init__(self):
+        pass
+
+class InterfaceRelatorios:
+    def __init__(self):
+        pass
+    
